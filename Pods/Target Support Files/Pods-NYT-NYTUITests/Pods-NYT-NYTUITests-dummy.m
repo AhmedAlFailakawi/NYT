5cf15d0c02b1cd64515189fc242f815a09770b08
Ahmed <86224939+AhmedAlFailakawi@users.noreply.github.com>
2022-11-25 13:10:43 +0300

    Delete Pods directory

--- Pods/Target Support Files/Pods-NYT-NYTUITests/Pods-NYT-NYTUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NYT_NYTUITests : NSObject
-@end
-@implementation PodsDummy_Pods_NYT_NYTUITests
-@end
--- Pods/Target Support Files/Pods-NYTTests/Pods-NYTTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NYTTests : NSObject
-@end
-@implementation PodsDummy_Pods_NYTTests
-@end
